73af07c87d120f1d6d4add38ab05e2fe0aac62db
Nofa Osamah <nofa.os111@gmail.com>
2025-07-07 11:10:50 +0300

    Solve the conflict

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,6 @@ use App\Models\Driver;
 ///home/u295987876/domains/sa-fvs.com/public_html/gh/app/Http/Controllers/AppController
 class AppController extends Controller
 {
-<<<<<<< HEAD
     public function updateProfile(Request $request)
 {
     $user = auth()->user();
@@ -42,70 +41,16 @@ class AppController extends Controller
                 ['driver_id' => $user->id], 
                 $validated                  
             );
-=======
-    //
-    public function sendVerificationCode(Request $request)
-    {
-        $request->validate([
-            'phone' => 'required|string',
-            'type' => 'required|in:client,driver',
-        ]);
 
-        $code = rand(1000, 9999);
-        $key = $request->type . '_' . $request->phone;
->>>>>>> e24dc0f2d07f7e4a1a4782bfe09c505e8769e210
-
-        Cache::put($key, $code, now()->addMinutes(5));
-
-        // SmsService::send($request->phone, "رمز التحقق الخاص بك هو: $code");
-
-        return response()->json(['message' => 'تم إرسال رمز التحقق بنجاح',
-                'code' => $code
-    ]);
-    }
-
-    public function verifyCode(Request $request)
-    {
-        $request->validate([
-            'phone' => 'required|string',
-            'code' => 'required|digits:4',
-            'type' => 'required|in:client,driver',
-        ]);
-
-        $key = $request->type . '_' . $request->phone;
-        $storedCode = Cache::get($key);
-
-        if (!$storedCode || $storedCode != $request->code) {
-            return response()->json(['message' => 'رمز التحقق غير صحيح أو منتهي'], 401);
-        }
-
-        Cache::forget($key);
-
-        if ($request->type === 'client') {
-            $user = Client::firstOrCreate([
-                'phone' => $request->phone,
-                'typeuser' => $request->type
-        ]);
-        } else {
-            $user = Driver::firstOrCreate(['phone' => $request->phone,
-            'typeuser' => $request->type
-        ]);
-        }
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
+    } else {
         return response()->json([
-            'message' => 'تم التحقق بنجاح',
-            'user' => $user,
-            'user_type' => $request->type,
-            'token' => $token,
-        ]);
+            'message' => 'نوع المستخدم غير مدعوم.',
+        ], 400);
     }
 
-    public function logout(Request $request)
-    {
-
-        $request->user()->tokens()->delete();
-        return response()->json(['message' => 'تم تسجيل الخروج']);
-    }
+    return response()->json([
+        'message' => 'تم تحديث البروفايل بنجاح.',
+        'profile' => $profile,
+    ]);
+}
 }
